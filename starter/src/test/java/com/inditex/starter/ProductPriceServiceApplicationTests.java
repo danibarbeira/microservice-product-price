8824ace25e501549b8a2ad03a3885024cf25377e
Daniel Barbeira Hayes <daniel.barbeira@udc.es>
2025-03-14 15:04:36 +0100

    refactor(architecture): create initial project structure

---
+++ starter/src/test/java/com/inditex/starter/ProductPriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPriceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
