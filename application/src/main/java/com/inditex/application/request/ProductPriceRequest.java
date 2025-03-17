54254ad91674b593fd19a3c25a35957cdef0d960
Daniel Barbeira Hayes <daniel.barbeira@udc.es>
2025-03-17 12:16:51 +0100

    feat(application): add validations to request/response
    Add fromDomain method to response record to transform domain object product to the desired response object.

--- application/src/main/java/com/inditex/application/request/ProductPriceRequest.java
+++ application/src/main/java/com/inditex/application/request/ProductPriceRequest.java
@@ -1,9 +1,11 @@
 package com.inditex.application.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
-public record ProductPriceRequest(@NotNull LocalDateTime requestDateTime, @NotNull Long productId,
+public record ProductPriceRequest(@NotNull @JsonFormat(pattern = "yyyy-MM-dd-HH.mm.ss") LocalDateTime requestDateTime,
+                                  @NotNull Long productId,
                                   @NotNull Integer brandId) {
 }
--- application/src/main/java/com/inditex/application/response/ProductPriceResponse.java
+++ application/src/main/java/com/inditex/application/response/ProductPriceResponse.java
@@ -1,13 +1,14 @@
 package com.inditex.application.response;
 
-import com.inditex.domain.model.Product;
+import com.inditex.domain.model.ProductPrice;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record ProductPriceResponse(Long productId, Integer brandId, Integer priceList, LocalDateTime startDate,
                                    LocalDateTime endDate, BigDecimal price) {
-    public static ProductPriceResponse fromDomain(Product product) {
+    public static ProductPriceResponse fromDomain(@NotNull ProductPrice product) {
         return new ProductPriceResponse(product.getProductId(), product.getBrandId(), product.getPriceList(), product.getStartDate(), product.getEndDate(), product.getPrice());
     }
 }
