c36f87d83e6ba1950fcb0a86ed0c8d7131afee77
Daniel Barbeira Hayes <daniel.barbeira@udc.es>
2025-03-17 14:12:36 +0100

    refactor(ProductPriceResponse)
    Refactor to Java class to support HATEOAS.

--- application/src/main/java/com/inditex/application/response/ProductPriceResponse.java
+++ application/src/main/java/com/inditex/application/response/ProductPriceResponse.java
@@ -2,12 +2,24 @@ package com.inditex.application.response;
 
 import com.inditex.domain.model.ProductPrice;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record ProductPriceResponse(Long productId, Integer brandId, Integer priceList, LocalDateTime startDate,
-                                   LocalDateTime endDate, BigDecimal price) {
+@AllArgsConstructor
+@Getter
+public class ProductPriceResponse extends RepresentationModel<ProductPriceResponse> {
+
+    private final Long productId;
+    private final Integer brandId;
+    private final Integer priceList;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+    private final BigDecimal price;
+
     public static ProductPriceResponse fromDomain(@NotNull ProductPrice product) {
         return new ProductPriceResponse(product.getProductId(), product.getBrandId(), product.getPriceList(), product.getStartDate(), product.getEndDate(), product.getPrice());
     }
