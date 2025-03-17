92eeca49a32ea9b7d49ed6063103608c65c9a2bc
Daniel Barbeira Hayes <daniel.barbeira@udc.es>
2025-03-17 14:16:36 +0100

    refactor(starter): config and scanning
    Add dependencies to pom for Jackson/JSON support, add component scan to starter to create all spring beans necessary, along with repositories and entities, add properties for h2 database support and initialization.

--- starter/src/main/java/com/inditex/starter/ProductPriceServiceApplication.java
+++ starter/src/main/java/com/inditex/starter/ProductPriceServiceApplication.java
@@ -2,12 +2,18 @@ package com.inditex.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.inditex.application", "com.inditex.infrastructure"})
+@EnableJpaRepositories("com.inditex.infrastructure.outbound.persistence.repositories")
+@EntityScan("com.inditex.infrastructure.outbound.persistence.entities")
 public class ProductPriceServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductPriceServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductPriceServiceApplication.class, args);
+    }
 
 }
