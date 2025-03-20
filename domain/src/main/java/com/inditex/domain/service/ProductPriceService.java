ae4929dba1cd752a8dda2b1a49083856348fc351
Daniel Barbeira Hayes <daniel.barbeira@udc.es>
2025-03-21 00:34:11 +0100

    refactor: reactive controller and service
    Modify Rest Controller and Service methos signatures and implementations to make them reactive, allowing non blocking i/o and asynchronous processing.

--- domain/src/main/java/com/inditex/domain/service/ProductPriceService.java
+++ domain/src/main/java/com/inditex/domain/service/ProductPriceService.java
@@ -2,9 +2,10 @@ package com.inditex.domain.service;
 
 import com.inditex.domain.exceptions.ProductPriceNotFoundException;
 import com.inditex.domain.model.ProductPrice;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
 public interface ProductPriceService {
-    ProductPrice findPriceForProductAtDate(Long productId, Integer brandId, LocalDateTime queryDate) throws ProductPriceNotFoundException;
+    Mono<ProductPrice> findPriceForProductAtDate(Long productId, Integer brandId, LocalDateTime queryDate) throws ProductPriceNotFoundException;
 }
--- infrastructure/src/main/java/com/inditex/infrastructure/inbound/api/ProductPriceRestController.java
+++ infrastructure/src/main/java/com/inditex/infrastructure/inbound/api/ProductPriceRestController.java
@@ -10,11 +10,11 @@ import lombok.val;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.BasicLinkBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/product")
@@ -23,11 +23,16 @@ public class ProductPriceRestController implements ProductPriceController {
 
     private final ProductPriceService productPriceService;
 
+    private ProductPriceResponse productPriceResponse = null;
+
     @PostMapping(value = "/price")
-    public ResponseEntity<ProductPriceResponse> obtainProductPrice(@RequestBody ProductPriceRequest productPriceRequest) {
+    public Mono<ProductPriceResponse> obtainProductPrice(@RequestBody ProductPriceRequest productPriceRequest) {
+
         val product = productPriceService.findPriceForProductAtDate(productPriceRequest.productId(), productPriceRequest.brandId(), productPriceRequest.requestDateTime());
 
-        return ResponseEntity.ok(toHateoas(ProductPriceResponse.fromDomain(product)));
+        product.subscribe(value -> productPriceResponse = toHateoas(ProductPriceResponse.fromDomain(value)));
+
+        return Mono.just(productPriceResponse);
     }
 
     private ProductPriceResponse toHateoas(@NotNull ProductPriceResponse productPriceResponse) {
